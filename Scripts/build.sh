568cee1fff5139f62d444889dea380768fb61d02
Terry Brady <terrywbrady+c9@georgetown.edu>
2018-04-01 21:45:25 +0000

    x

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-cd /home/user/dspace-src/DSpace
+
+DSPACE_SRC=/home/user/dspace_src
+DSPACE_INSTALL=/home/user/dspace
+LOCAL_CFG=/projects/DSpace-Codenvy/local.cfg
+MVN_TARGET=package
+MVN_TARGET=package -Dmirage2.on=true
+DSPACE_VER=6
+START_TOMCAT=$1
+
+echo $START_TOMCAT
+exit
+
+cd ${DSPACE_SRC}
 mvn package
 # mvn package -Dmirage2.on=true
 cd dspace/target/dspace-installer
