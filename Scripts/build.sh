dbb6ef28a1532d0b27b8f497ef23937036cdb605
Terry Brady <terrywbrady+c9@georgetown.edu>
2018-04-01 22:17:12 +0000

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-DSPACE_SRC=/home/user/dspace_src
+function die {
+    echo $1
+    exit 10
+}
+
+DSPACE_SRC=/home/user/dspace-src
 DSPACE_INSTALL=/home/user/dspace
-LOCAL_CFG=/projects/DSpace-Codenvy/local.cfg
+LOCAL_CFG=/projects/DSpace-codenvy/CodenvyConfig/local.cfg
 MVN_TARGET=package
-MVN_TARGET=package -Dmirage2.on=true
+#MVN_TARGET="package -Dmirage2.on=true"
 DSPACE_VER=6
-START_TOMCAT=$1
 
-echo $START_TOMCAT
-exit
+START_TOMCAT=${1:-0}
+TOMCAT=/home/user/tomcat8/bin/catalina.sh
 
-cd ${DSPACE_SRC}
-mvn package
-# mvn package -Dmirage2.on=true
-cd dspace/target/dspace-installer
+cd ${DSPACE_SRC}/DSpace || die "src dir ${DSPACE_SRC} does not exist"
+cp ${LOCAL_CFG} . || die "error copying local: ${LOCAL_CFG}"
+mvn ${MVN_TARGET} || die "maven failed"
+cd dspace/target/dspace-installer || die "install dir not found"
+cp /projects/DSpace-codenvy/overlay/* webapps || die "could not copy webapps overlay"
+
+${TOMCAT} stop
+
+ant update
+
+if [ $START_TOMCAT != 0 ]
+then
+  ${TOMCAT} stop
+fi
+
 
